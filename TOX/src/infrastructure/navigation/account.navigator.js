fcb449d0813656f9f63b325cf5c48476a547f740
Aryan486 <76522834+Aryan486@users.noreply.github.com>
2022-11-20 21:14:17 +0530

    transition presets account navigator

--- TOX/src/infrastructure/navigation/account.navigator.js
+++ TOX/src/infrastructure/navigation/account.navigator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 import { AccountScreen } from "../../features/account/screens/account.screen";
 import { LoginScreen } from "../../features/account/screens/login.screen";
 import { RegisterScreen } from "../../features/account/screens/register.screen";
@@ -8,7 +8,7 @@ import { ForgotPassword } from "../../features/account/screens/forgotPassword.sc
 const Stack = createStackNavigator();
 
 export const AccountNavigator = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }} >
+  <Stack.Navigator screenOptions={{ headerShown: false, ...TransitionPresets.SlideFromRightIOS }} >
     <Stack.Screen name="Main" component={AccountScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
